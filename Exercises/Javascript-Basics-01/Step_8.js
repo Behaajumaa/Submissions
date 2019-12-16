b4d2dcda1b3750c3906cef3080455c72f71fd885
Behaajumaa <mr.behaajumaa@gmail.com>
2019-12-16 16:24:14 +0200

    step8 in Basics-01 is DONE

---
+++ Exercises/Javascript-Basics-01/Step_8.js
@@ -0,0 +1,13 @@
+function yourage(){
+    var x = document.getElementById('age').value;
+   //console.log(x)
+    //var year =
+
+    if (x > 18){
+        alert ("You are over 18 ");
+}
+    else{
+        alert ("You are under 18 ");
+}
+ 
+}
