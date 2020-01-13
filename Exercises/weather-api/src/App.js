397254ed8fa3dfdff39fb0e366bb767cf9adee4a
Behaajumaa <mr.behaajumaa@gmail.com>
2020-01-13 15:41:53 +0200

    finfish it

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import mostlycloudy from './img/weather-icons/mostlycloudy.svg';
+import sunny from './img/weather-icons/clear.svg';
+
+
+
+
+
 
 class App extends Component {
   constructor(props) {
@@ -9,11 +16,55 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <header className="app__header"></header>
-        <main className="app__main"></main>
-        <h2>Behaa Jumaa</h2> 
-      </div>
+    
+<div className="app">
+
+        <div className="header">
+         <input className="inputCity" type="text" placeholder="Type a city name"></input><button className="ButtonFindWeathe"> FindWeather</button>
+        </div>
+
+
+        <div className="main">
+
+            <div className="imge"><img   className="image" src={mostlycloudy} alt="mostlycloudy" ></img></div>
+          
+            <div> <p className="pElaments ">overcast clouds</p>               </div>
+            <div> <p > <strong>Temperature</strong> 10&#176;
+ to 11&#8451; </p> </div>
+            <div> <p ><strong>Humidity </strong>&nbsp;
+ 78% &nbsp;
+<strong>Pressure&nbsp; &nbsp;
+
+</strong> 1008.48</p> </div>
+<div></div>
+
+        </div>
+
+
+        <div className="footer">      
+        <div className="smallimg">   </div>
+
+        <div className="smallimg"> <p>03:00</p>  <img  src={mostlycloudy} alt="smallimg" ></img> <p>8&#8451;</p>   </div>
+        <div className="smallimg"> <p>06:00</p>  <img  src={mostlycloudy} alt="smallimg" ></img> <p>9&#8451;</p>   </div>
+        <div className="smallimg"> <p>09:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>14&#8451;</p>   </div>
+        <div className="smallimg"> <p>12:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>17&#8451;</p>   </div>
+        <div className="smallimg"> <p>15:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>18&#8451;</p>   </div>
+        <div className="smallimg"> <p>18:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>16&#8451;</p>   </div>
+        <div className="smallimg"> <p>21:00</p>  <img  src={mostlycloudy} alt="smallimg" ></img> <p>13&#8451;</p>   </div>
+
+        <div className="smallimg">   </div>
+
+        
+        
+        
+        </div>
+ 
+
+
+
+</div> 
+
+
     );
   }
 }
