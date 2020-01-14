6aeec8c518f855de08909d3713137de63fee0560
Behaajumaa <mr.behaajumaa@gmail.com>
2020-01-14 10:54:06 +0200

    update

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import mostlycloudy from './img/weather-icons/mostlycloudy.svg';
-import sunny from './img/weather-icons/clear.svg';
-
-
-
-
-
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import clear from "./img/weather-icons/clear.svg";
 
 class App extends Component {
   constructor(props) {
@@ -16,57 +11,75 @@ class App extends Component {
 
   render() {
     return (
-    
-<div className="app">
-
+      <div className="app">
         <div className="header">
-         <input className="inputCity" type="text" placeholder="Type a city name"></input><button className="ButtonFindWeathe"> FindWeather</button>
+          <input
+            type="text"
+            placeholder="Type in a city name"
+            className="inputcity"
+          ></input>
+          <button className="ButtonFindWeathe"> FindWeather</button>
         </div>
-
-
         <div className="main">
-
-            <div className="imge"><img   className="image" src={mostlycloudy} alt="mostlycloudy" ></img></div>
-          
-            <div> <p className="pElaments ">overcast clouds</p>               </div>
-            <div> <p > <strong>Temperature</strong> 10&#176;
- to 11&#8451; </p> </div>
-            <div> <p ><strong>Humidity </strong>&nbsp;
- 78% &nbsp;
-<strong>Pressure&nbsp; &nbsp;
-
-</strong> 1008.48</p> </div>
-<div></div>
-
+          <div className="imge">
+            <img src={mostlycloudy} alt="mostlycloudy"></img>
+          </div>
+          <div>
+            <p className="first">overcast clouds</p>
+          </div>
+          <div>
+            <p>
+              <strong>Temperature 10&#176; to 11&#8451;</strong>
+            </p>
+          </div>
+          <div>
+            <p>
+              <strong>Humidity &nbsp;</strong> 78%{" "}
+              <strong>&nbsp;&nbsp;Pressure&nbsp;</strong> 1008.48
+            </p>
+          </div>
         </div>
-
-
-        <div className="footer">      
-        <div className="smallimg">   </div>
-
-        <div className="smallimg"> <p>03:00</p>  <img  src={mostlycloudy} alt="smallimg" ></img> <p>8&#8451;</p>   </div>
-        <div className="smallimg"> <p>06:00</p>  <img  src={mostlycloudy} alt="smallimg" ></img> <p>9&#8451;</p>   </div>
-        <div className="smallimg"> <p>09:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>14&#8451;</p>   </div>
-        <div className="smallimg"> <p>12:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>17&#8451;</p>   </div>
-        <div className="smallimg"> <p>15:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>18&#8451;</p>   </div>
-        <div className="smallimg"> <p>18:00</p>  <img  src={sunny}        alt="smallimg" ></img> <p>16&#8451;</p>   </div>
-        <div className="smallimg"> <p>21:00</p>  <img  src={mostlycloudy} alt="smallimg" ></img> <p>13&#8451;</p>   </div>
-
-        <div className="smallimg">   </div>
-
-        
-        
-        
+        <div className="footer">
+          <div className="small"></div>
+          <div className="small">
+            <p>03:00</p>
+            <img src={mostlycloudy} alt="mostlycloudy"></img>
+            <p>8&#8451;</p>
+          </div>
+          <div className="small">
+            <p>06:00</p>
+            <img src={mostlycloudy} alt="mostlycloudy"></img>
+            <p>9&#8451;</p>
+          </div>
+          <div className="small">
+            <p>09:00</p>
+            <img src={clear} alt="clear"></img>
+            <p>14&#8451;</p>
+          </div>
+          <div className="small">
+            <p>12:00</p>
+            <img src={clear} alt="clear"></img>
+            <p>17&#8451;</p>
+          </div>
+          <div className="small">
+            <p>15:00</p>
+            <img src={clear} alt="clear"></img>
+            <p>18&#8451;</p>
+          </div>
+          <div className="small">
+            <p>18:00</p>
+            <img src={clear} alt="clear"></img>
+            <p>16&#8451;</p>
+          </div>
+          <div className="small">
+            <p>21:00</p>
+            <img src={mostlycloudy} alt="mostlycloudy"></img>
+            <p>13&#8451;</p>
+          </div>
+          <div className="small"></div>
         </div>
- 
-
-
-
-</div> 
-
-
+      </div>
     );
   }
 }
-
 export default App;
