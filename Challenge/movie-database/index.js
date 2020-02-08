936a417678ce293302d0ebc2cf48acc1bc27be6d
Behaa_Jumaa <behaa@arch.localdomain>
2020-02-08 22:05:47 +0200

    step2

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,27 +1,20 @@
 const express = require("express");
 const app = express();
-const port = "3035";
+const port = "3025";
 //routes
+
 app.get("/", (req, res) => {
   res.send("Hello World from Behaa :)");
 });
-
-
-
 
 app.get('/time',  (req, res) => {
     res.send({status:200, message:'12:32'});
   });
 
-
-
   app.get('/test',  (req, res)=> {
     res.send({status:200, message:'Ok'});
   });
-
-
-
-
+  
 app.listen(port, () => {
     console.log(`server is running at: http://localhost:${port}/`);
   });
